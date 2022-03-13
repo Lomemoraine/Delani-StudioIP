3fb728f6671a157347d8e5a78ebc09cb69aea44c
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-03-13 23:55:06 +0300

    add toggle effect on what we do section

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#image1,#paragraph1").click(function(){
+        $("#image1,#paragraph1").toggle();
+      });
+      $("#image2,#paragraph2").click(function(){
+        $("#image2,#paragraph2").toggle();
+      });
+      $("#image3,#paragraph3").click(function(){
+        $("#image3,#paragraph3").toggle();
+      });
+})
+
